eacca6f2f5ae3c9c688fe1eee1cb66704ca181ec
jvallee <jasonmvallee@gmail.com>
2021-01-22 14:20:17 -0600

    unused imports

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import { DoverOnsiteCodingChallengeBackendAPI } from "./swaggerapi/doverOnsiteCodingChallengeBackendAPI";
-import { Container, Row } from "reactstrap";
 import OutReachEditorPage from "./components/pages/OutReach/OutReach";
 import Header from "./components/common/Header/Header";
 
